f6e14688a39124876d7f2b6d0b64af4fba08f8c2
javeria2108 <javeriazh@gmail.com>
2023-07-27 18:51:46 +0500

    .

--- src/components/Portfolio/Projects.jsx
+++ src/components/Portfolio/Projects.jsx
@@ -10,7 +10,7 @@ const Projects = () => {
              <div key={index} className='bg-black bg-opacity-60 py-3 flex flex-col w-11/12 sm:w-3/4  items-center text-center my-10 mx-auto px-5 sm:px-10  rounded-2xl shadow-inner shadow-navbar border-b border-b-navbar'>
              <h3 className='font-coolvetica py-3'>{project.title}</h3>
              <p className='text-2xl py-2'>{project.description}</p>
-             <div className='image-container w-62 h-48 overflow-hidden'>
+             <div className='image-container w-full sm:w-62 h-48 overflow-hidden'>
              <img src={project.imageUrl} alt={project.title}
              className='w-full h-full object-cover ' />
                </div>
