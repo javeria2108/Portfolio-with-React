b24a9894f491a199884e18a40e188c893a659de9
javeria2108 <javeriazh@gmail.com>
2023-07-27 18:54:34 +0500

    .

--- src/components/Portfolio/Projects.jsx
+++ src/components/Portfolio/Projects.jsx
@@ -1,5 +1,24 @@
 import React from 'react'
-import { ProjectsData } from './projectsData'
+const ProjectsData=[
+    {
+        title:"Quran World",
+        description: "An interactive and easy to use Quran app made with React, Redux and Tailwind CSS.",
+        imageUrl: "src/assets/images/QuranWorld.png",
+        githubLink:"https://github.com/javeria2108/Quran-app-with-React-and-Redux"
+    },
+    {
+        title:"Weather App",
+        description: "A javascript weather app which displays weather data of any city",
+        imageUrl: "src/assets/images/WeatherApp.png",
+        githubLink:"https://github.com/javeria2108/Javascript-Weather-App"
+    },
+    {
+        title:"Pokedox",
+        description: "A pokemon search app made with React and Axios",
+        imageUrl: "src/assets/images/Pokedox.png",
+        githubLink:"https://github.com/javeria2108/Pokemon-Search-App-with-React"
+    }
+]
 const Projects = () => {
   return (
     <div >
