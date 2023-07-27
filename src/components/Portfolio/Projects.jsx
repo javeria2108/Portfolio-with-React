681edd2b675d4aac8ba33a196617057e329a0251
javeria2108 <javeriazh@gmail.com>
2023-07-27 18:47:46 +0500

    width change

--- src/components/Portfolio/Projects.jsx
+++ src/components/Portfolio/Projects.jsx
@@ -6,8 +6,8 @@ const Projects = () => {
          <div className='flex flex-col items-center justify-center
          mt-5 pt-10 text-2xl sm:text-4xl font-helvetica bg-stars'>
             <h1 className='mb-4'>{"<"} Projects {"/>"}</h1>
-        {ProjectsData.map((project)=>(
-             <div className='bg-black bg-opacity-60 py-3 flex flex-col w-full sm:w-3/4  items-center text-center my-10 mx-auto px-5 sm:px-10  rounded-2xl shadow-inner shadow-navbar border-b border-b-navbar'>
+        {ProjectsData.map((project,index)=>(
+             <div key={index} className='bg-black bg-opacity-60 py-3 flex flex-col w-11/12 sm:w-3/4  items-center text-center my-10 mx-auto px-5 sm:px-10  rounded-2xl shadow-inner shadow-navbar border-b border-b-navbar'>
              <h3 className='font-coolvetica py-3'>{project.title}</h3>
              <p className='text-2xl py-2'>{project.description}</p>
              <div className='image-container w-62 h-48 overflow-hidden'>
