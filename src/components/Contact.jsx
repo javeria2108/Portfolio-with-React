bd46a4b6d21f18d6b68e3cd648890d79afbbf2a9
javeria2108 <javeriazh@gmail.com>
2023-07-30 21:21:01 +0500

    contact form added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+       <div className='flex flex-col items-center justify-center
+         mt-5 pt-10 text-2xl sm:text-4xl font-helvetica bg-stars mb-5'>
+        <div className='flex flex-col w-full sm:w-3/4 font-helvetica  text-center  m-auto px-5 py-5 sm:px-10 bg- rounded-2xl shadow-inner shadow-navbar border-b border-b-navbar'>
+     <h1>{"<"} Contact Me {"/>"}</h1>
+     <p className='py-2 text:2xl sm:text-3xl'>You can send me an Email at <br/> 
+     javeriazh@gmail.com</p>
+      <form className='mx-3 my-5' action="">
+        <div></div>
+        <input type='text' placeholder='Name..'
+        className='bg-black text-white placeholder:text-white my-3 px-8 py-2 rounded-full opacity-60'></input>
+       <input type='email' placeholder='Email..'
+        className='bg-black text-white placeholder:text-white my-3 px-8 py-2 rounded-full opacity-60'></input>
+      <textarea placeholder='type a message...'
+       className='bg-black text-white placeholder:text-white my-3 px-8 py-2 rounded-lg opacity-60'></textarea>
+       <button className='w-48 sm:w-56 font-helvetica text-xl sm:text-2xl my-4
+               p-2 sm:p-3 sm:my-5 border border-white   hover:-translate-y-1 hover:scale-110 duration-300' >Send</button>
+      </form>
+      </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import ProgressBar from '@ramonak/react-progress-bar'
+const colors=[,,,,]
+const skills=[{name:"Html",percent:90,color:"#2F0048"},{name:"CSS",percent:75,color:"#4634B9"},{name:"Javascript",percent:90,color:"#777777"}
+,{name:"React Js",percent:88,color:"#12006E"},{name:"Tailwind CSS",percent:80,color:"#12006E"},{name:"Redux",percent:85,color:"#00D5FA"},
+{name:"Recoil",percent:75,color:"#8CC84B"},{name:"Styled Components",percent:80,color:"#00D5FA"}]
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div className='flex flex-col items-center justify-center
+    mt-5 pt-10 text-2xl sm:text-4xl font-helvetica'>
+      <h1 className='mb-4'>{"<"} Skills {"/>"}</h1>
+      <div className='flex flex-col w-full sm:w-3/4 font-helvetica  text-center  m-auto px-5 py-5 sm:px-10 bg- rounded-2xl shadow-inner shadow-navbar border-b border-b-navbar'>
+        {skills.map((skill,index)=>(
+          <div className='m-5 md:mx-10 flex flex-row justify-between' key={index}>
+            <h1 className=''>{skill.name}</h1>
+            <ProgressBar className= 'w-40 sm:w-48 md:w-64' completed = {skill.percent} bgColor = {skill.color} animateOnRender = {true} />
+          </div>
+        ))}
+        </div>
+    </div>
   )
 }
 
